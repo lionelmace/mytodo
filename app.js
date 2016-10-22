2e80b53f92cb2dcdb0d110a59df5d2ecfe48031c
Lionel Mace <lionel.mace@fr.ibm.com>
2016-10-22 23:49:36 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var cfenv   = require('cfenv');
+var favicon = require('serve-favicon');
+var app     = express();
+var bodyParser = require('body-parser')
+
+// load local VCAP configuration
+var vcapLocal = null
+try {
+  vcapLocal = require("./vcap-local.json");
+  console.log("Loaded local VCAP", vcapLocal);
+} catch (e) {
+  console.error(e);
+}
+
+// This option property is ignored if not running locally.
+var options = vcapLocal ? { vcap: vcapLocal } : {}
+var appEnv = cfenv.getAppEnv(options);
+
+console.log('BASE_INFO');
+console.log('---------');
+console.log('Is Local: ' + appEnv.isLocal);
+console.log('App Name: ' + appEnv.name);
+
+// load the services bound to this application
+console.log('SERVICES');
+console.log('--------');
+var services = appEnv.getServices();
+var dbServiceName;
+//console.log(services);
+var count = 0;
+for (var serviceName in services) {
+  if (services.hasOwnProperty(serviceName)) {
+    count++;
+    var service = services[serviceName];
+    console.log('Name=' + service.name + ' - Label=' + service.label);
+    if (service.label == "cloudantNoSQLDB") {
+      dbServiceName =  service.name;
+    }
+  }
+}
+if (!count) {
+  console.log('No services are bound to this app.\n');
+}
+
+// expect a service whose name matches the regular expression
+//console.log('toto ' + appEnv.getServiceCreds(/cloudant/i));
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json()); // parse application/json
+
+require("./app/database.js")(appEnv, dbServiceName, "todos", 
+  function (err, database) {
+    if (err) {
+      console.log(err);
+    } else {
+      // database is initialized, install our CRUD route for Todo objects
+      require('./app/todos.js')(app, database);
+    }
+});
+
+// set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public'));
+app.use(favicon(__dirname + '/public/icons/favicon.ico'));
+
+// start server on the specified port and binding host
+app.listen(appEnv.port, "0.0.0.0", function () {
+  console.log("Server starting on " + appEnv.url);
+});
