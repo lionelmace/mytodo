1908974ba2ae081b65581b55f015821db2d5ee41
Lionel Mace <lionel.mace@gmail.com>
2019-04-09 00:38:55 +0200

    tf update

--- terraform/iks-cluster-multizone.tf
+++ terraform/iks-cluster-multizone.tf
@@ -1,5 +1,5 @@
-variable "environment_name" {
-  default = "terraform-env"
+variable "cluster_name" {
+  default = "terraform-env-hacluster"
 }
 
 # The datacenter of the worker nodes
@@ -67,57 +67,63 @@ variable "cluster_kube_version" {
 }
 
 resource "ibm_container_cluster" "azcluster" {
-  name              = "${var.environment_name}-azcluster"
+  name              = "${var.cluster_name}"
   datacenter        = "${var.cluster_datacenter}"
   hardware = "shared"
   machine_type      = "${var.cluster_machine_type}"
   region            = "${var.ibmcloud_region}"
+  kube_version      = "${var.cluster_kube_version}"
   public_vlan_id    = "${var.az1_vlan_id_public}"
   private_vlan_id   = "${var.az1_vlan_id_private}"
+  resource_group_id = "${data.ibm_resource_group.group.id}"
   # default_pool_size = 1
 }
 
-# resource "ibm_container_worker_pool" "test_pool" {
-#   # cluster          = "${var.environment_name}-azcluster"
-#   cluster          = "${ibm_container_cluster.azcluster.id}"
-#   worker_pool_name = "my_pool"
-#   machine_type     = "u2c.2x4"
-#   size_per_zone    = 1
-#   hardware         = "shared"
-#   disk_encryption  = "true"
-#   region = "eu-de"
-#   labels = {
-#     "test" = "test-pool"
-#     "test1" = "test-pool1"
-#   }
-# }
+resource "ibm_container_worker_pool" "test_pool" {
+  cluster          = "${ibm_container_cluster.azcluster.id}"
+  worker_pool_name = "default"
+  machine_type     = "u2c.2x4"
+  size_per_zone    = 1
+  hardware         = "shared"
+  disk_encryption  = "true"
+  region           = "${var.ibmcloud_region}"
+  labels = {
+    "test" = "test-pool"
+    "test1" = "test-pool1"
+  }
+}
 
 resource ibm_container_worker_pool_zone_attachment az1 {
   # cluster         = "${var.environment_name}-azcluster"
-  cluster         = "${ibm_container_cluster.azcluster.id}"
-  worker_pool     = "default"
-  zone            = "${var.az1}"
-  public_vlan_id  = "${var.az1_vlan_id_public}"
-  private_vlan_id = "${var.az1_vlan_id_private}"
-  region          = "${var.ibmcloud_region}"
+  cluster           = "${ibm_container_cluster.azcluster.id}"
+  worker_pool       = "default"
+  zone              = "${var.az1}"
+  public_vlan_id    = "${var.az1_vlan_id_public}"
+  private_vlan_id   = "${var.az1_vlan_id_private}"
+  region            = "${var.ibmcloud_region}"
+  resource_group_id = "${data.ibm_resource_group.group.id}"
 }
 resource ibm_container_worker_pool_zone_attachment az2 {
   # cluster         = "${var.environment_name}-azcluster"
-  cluster         = "${ibm_container_cluster.azcluster.id}"
-  worker_pool     = "default"
-  zone            = "${var.az2}"
-  public_vlan_id  = "${var.az2_vlan_id_public}"
-  private_vlan_id = "${var.az2_vlan_id_private}"
-  region          = "${var.ibmcloud_region}"
+  cluster           = "${ibm_container_cluster.azcluster.id}"
+  worker_pool       = "default"
+  zone              = "${var.az2}"
+  public_vlan_id    = "${var.az2_vlan_id_public}"
+  private_vlan_id   = "${var.az2_vlan_id_private}"
+  region            = "${var.ibmcloud_region}"
+  resource_group_id = "${data.ibm_resource_group.group.id}"
+  depends_on        = ["ibm_container_worker_pool_zone_attachment.az1"]
 }
 resource ibm_container_worker_pool_zone_attachment az3 {
-  # cluster          = "${var.environment_name}-azcluster"
-  cluster         = "${ibm_container_cluster.azcluster.id}"
-  worker_pool     = "default"
-  zone            = "${var.az3}"
-  public_vlan_id  = "${var.az3_vlan_id_public}"
-  private_vlan_id = "${var.az3_vlan_id_private}"
-  region          = "${var.ibmcloud_region}"
+  # cluster         = "${var.environment_name}-azcluster"
+  cluster           = "${ibm_container_cluster.azcluster.id}"
+  worker_pool       = "default"
+  zone              = "${var.az3}"
+  public_vlan_id    = "${var.az3_vlan_id_public}"
+  private_vlan_id   = "${var.az3_vlan_id_private}"
+  region            = "${var.ibmcloud_region}"
+  resource_group_id = "${data.ibm_resource_group.group.id}"
+  depends_on        = ["ibm_container_worker_pool_zone_attachment.az2"]
 
   //User can increase timeouts
   # timeouts {
