206e77af8cf8d792a4a795fc8292aba7a1df9b43
Lionel Mace <lionel.mace@gmail.com>
2019-04-30 16:04:13 +0200

    updated yaml

--- terraform/global-variables.tf
+++ terraform/global-variables.tf
@@ -1,5 +1,17 @@
-variable "ibmcloud_location" {
+variable "ibmcloud_region" {
   default = "eu-de"
+}
+
+variable "ibmcloud_az1" {
+  default = "fra02"
+}
+
+variable "ibmcloud_az2" {
+  default = "fra04"
+}
+
+variable "ibmcloud_az3" {
+  default = "fra05"
 }
 
 data "ibm_resource_group" "group" {
