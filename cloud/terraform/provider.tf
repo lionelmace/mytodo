eb49f541ea1652624bce52a53fa5fe3fc5ccfd9e
Lionel Mace <lionel.mace@gmail.com>
2021-12-13 18:35:31 +0100

    downgrade terraform to 1.0.7

--- cloud/terraform/provider.tf
+++ cloud/terraform/provider.tf
@@ -4,11 +4,11 @@
 ##############################################################################
 
 terraform {
-  required_version = ">= 1.0.11"
+  required_version = ">= 1.0.7"
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.36.0"
+      version = "1.37.0"
     }
   }
 }
