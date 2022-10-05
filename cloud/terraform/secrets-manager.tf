4dcc8c6fbdebc0ba04fc4b5432d3f88c49de8973
Lionel Mace <lionel.mace@gmail.com>
2022-10-05 07:34:43 +0200

    removed secrets manager standard plan

--- cloud/terraform/secrets-manager.tf
+++ cloud/terraform/secrets-manager.tf
@@ -4,16 +4,6 @@
 resource "ibm_resource_instance" "secrets-manager" {
   name              = "${var.prefix}-secrets-manager"
   service           = "secrets-manager"
-  plan              = "standard"
-  location          = var.region
-  resource_group_id = ibm_resource_group.resource_group.id
-  tags              = var.tags
-  service_endpoints = "private"
-}
-
-resource "ibm_resource_instance" "secrets-manager-trial" {
-  name              = "${var.prefix}-secrets-manager"
-  service           = "secrets-manager"
   plan              = "trial"
   location          = var.region
   resource_group_id = ibm_resource_group.resource_group.id
