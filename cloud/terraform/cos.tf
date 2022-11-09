6a6954e386153711b5467b7f2f83670f5611aae2
Lionel Mace <lionel.mace@gmail.com>
2022-11-09 12:25:59 +0100

    removed cos module

--- cloud/terraform/cos.tf
+++ cloud/terraform/cos.tf
@@ -3,23 +3,16 @@
 # COS Service for OpenShift Internal Registry
 ##############################################################################
 
-module "cos" {
-  source = "terraform-ibm-modules/cos/ibm//modules/instance"
-
+resource "ibm_resource_instance" "cos" {
+  name = "${var.prefix}-openshift-registry"
+  service = var.cos_plan
+  location = var.cos_region
   resource_group_id = ibm_resource_group.resource_group.id
-  service_name      = "${var.prefix}-openshift-registry"
-  plan              = var.cos_plan
-  region            = var.cos_region
   tags              = var.tags
   key_tags          = var.tags
-  # service_endpoints = var.service_endpoints
-  # resource_key_name = var.resource_key_name
-  # role              = var.role
-  # bind_resource_key = var.bind_resource_key
-  # key_parameters    = var.key_parameters
 }
 
 output "cos_instance_crn" {
   description = "The CRN of the COS instance"
-  value       = module.cos.cos_instance_id
+  value       = ibm_resource_instance.cos.cos_instance_id
 }
