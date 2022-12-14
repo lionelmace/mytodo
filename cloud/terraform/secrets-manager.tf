0bca677e668fcb6141cb8c7111be412f1084fa47
Lionel Mace <lionel.mace@gmail.com>
2022-12-14 14:43:18 +0100

    added iam authorization between SM and vpn

--- cloud/terraform/secrets-manager.tf
+++ cloud/terraform/secrets-manager.tf
@@ -1,3 +1,13 @@
+# Authorization policy between VPN and Secrets Manager
+resource "ibm_iam_authorization_policy" "vpn-sm" {
+  source_service_name         = "vpn-server"
+  # source_resource_instance_id = 
+  target_service_name         = "secrets-manager"
+  target_resource_instance_id = ibm_resource_instance.secrets-manager.guid
+  roles                       = ["SecretsReader"]
+}
+
+
 ##############################################################################
 ## Secrets Manager
 ##############################################################################
