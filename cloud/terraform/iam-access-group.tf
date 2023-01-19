41bfaa4afbea4b119f8aa640d19f3bf01afb1e92
Lionel Mace <lionel.mace@gmail.com>
2023-01-19 17:11:09 +0100

    added rg viewer policy

--- cloud/terraform/iam-access-group.tf
+++ cloud/terraform/iam-access-group.tf
@@ -3,6 +3,15 @@ resource "ibm_iam_access_group" "accgrp" {
   tags = var.tags
 }
 
+resource "ibm_iam_access_group_policy" "iam-rg-viewer" {
+  access_group_id = ibm_iam_access_group.accgrp.id
+  roles           = ["Viewer"]
+  resources {
+    resource_type = "resource-group"
+    resource      = ibm_resource_group.resource_group.id
+  }
+}
+
 # Create a policy to all Kubernetes instances within the Resource Group
 resource "ibm_iam_access_group_policy" "policy-k8s" {
   access_group_id = ibm_iam_access_group.accgrp.id
