db7845342549614b40a811d4288c6cfb42b8ae80
Lionel Mace <lionel.mace@gmail.com>
2023-01-31 15:25:41 +0100

    use COS private endpoint for SCC

--- cloud/terraform/storage-cos.tf
+++ cloud/terraform/storage-cos.tf
@@ -48,13 +48,13 @@ resource "ibm_resource_instance" "cos" {
 
 ## COS Bucket
 ##############################################################################
-# SCC requires Cross-Region bucket for bucket resiliency
+# SCC requires Cross-Region bucket for resiliency
 resource "ibm_cos_bucket" "a-smart-bucket" {
   bucket_name           = "cos-bucket-for-scc"
   resource_instance_id  = ibm_resource_instance.cos.id
   storage_class         = "smart"
-  cross_region_location = "eu"
-  # region_location      = "eu-de"
+  # cross_region_location = "eu"
+  region_location      = "eu-de"
   activity_tracking {
     read_data_events     = true
     write_data_events    = true
@@ -65,6 +65,7 @@ resource "ibm_cos_bucket" "a-smart-bucket" {
     request_metrics_enabled = true
     metrics_monitoring_crn  = module.monitoring_instance.id
   }
+  endpoint_type = "private"
   # allowed_ip = ["223.196.168.27", "223.196.161.38", "192.168.0.1"]
 }
 
