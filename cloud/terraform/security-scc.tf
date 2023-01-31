8d06a6e1b28dce165fa4341beed6dfe1c2afd385
Lionel Mace <lionel.mace@gmail.com>
2023-01-31 10:22:28 +0100

    clean tf scc

--- cloud/terraform/security-scc.tf
+++ cloud/terraform/security-scc.tf
@@ -6,7 +6,7 @@ resource "ibm_scc_posture_collector" "scc_collector" {
   description = "sample collector"
   is_public   = true
   managed_by  = "ibm"
-  name        = "${var.prefix}-collector"
+  name        = format("%s-%s", var.prefix, "collector")
 }
 
 ## SCC Credentials
@@ -18,7 +18,7 @@ resource "ibm_scc_posture_credential" "scc_credential" {
     ibm_api_key = var.ibmcloud_api_key
   }
   enabled = true
-  name    = "${var.prefix}-credentials"
+  name    = format("%s-%s", var.prefix, "credentials")
   purpose = "discovery_fact_collection" # "discovery_fact_collection_remediation"
   type    = "ibm_cloud"
 }
@@ -27,14 +27,13 @@ resource "ibm_scc_posture_credential" "scc_credential" {
 ##############################################################################
 
 resource "ibm_scc_posture_scope" "scc_scope" {
-  collector_ids = [ibm_scc_posture_collector.scc_collector.id]
-  #   collector_ids   = ["${ibm_scc_posture_collector.scc_collector.id}"]
+  collector_ids   = [ibm_scc_posture_collector.scc_collector.id]
   credential_id   = ibm_scc_posture_credential.scc_credential.id
   credential_type = "ibm"
   description     = "IBMSchema"
   #   interval = 10
   #   is_discovery_scheduled = true
-  name = "${var.prefix}-scope"
+  name = format("%s-%s", var.prefix, "scope")
 }
 
 ## SCC Scan
@@ -52,10 +51,10 @@ resource "ibm_scc_posture_scan_initiate_validation" "scc_scan" {
   scope_id = ibm_scc_posture_scope.scc_scope.id
   # IBM Cloud Security Best Practices (profile_id=19)
   profile_id = data.ibm_scc_posture_profile.profile_security_bestpractices.profile_id
-  name       = "${var.prefix}-scan"
+  name       = format("%s-%s", var.prefix, "scan")
   # For On-Demand scan, comment the frequency
   # Minimum scan frequency limit is 1 hour (= 3600 msec)
-  frequency = 3600
+  # frequency = 3600
   #   no_of_occurrences = 1
 }
 
@@ -65,11 +64,11 @@ data "ibm_scc_posture_profile" "profile_fscloud" {
 }
 
 resource "ibm_scc_posture_scan_initiate_validation" "scc_scan_fscloud" {
-  scope_id = ibm_scc_posture_scope.scc_scope.id
+  scope_id   = ibm_scc_posture_scope.scc_scope.id
   profile_id = data.ibm_scc_posture_profile.profile_fscloud.profile_id
-  name       = "${var.prefix}-scan-fscloud"
+  name       = format("%s-%s", var.prefix, "scan-fscloud")
   # For On-Demand scan, comment the frequency
   # Minimum scan frequency limit is 1 hour (= 3600 msec)
-  frequency = 3600
+  # frequency = 3600
   #   no_of_occurrences = 1
 }
