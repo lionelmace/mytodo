ea1e5f2166a553bb5f1f3527397b805d410b4cb0
Lionel Mace <lionel.mace@gmail.com>
2023-03-23 11:41:34 +0100

    added cloudflare ips

--- cloud/terraform/vpc-network.tf
+++ cloud/terraform/vpc-network.tf
@@ -41,6 +41,88 @@ variable "vpc_acl_rules" {
   ]
 }
 
+# CIS / Cloudlfare IP Ranges 
+# https://api.cis.cloud.ibm.com/v1/ips
+variable "vpc_sg_cloudflare_rules" {
+  default = [
+    {
+      direction   = "inbound"
+      ip_version  = "IPv4"
+      remote      = "173.245.48.0/20"
+    },
+    {
+      direction   = "inbound"
+      ip_version  = "IPv4"
+      remote      = "103.21.244.0/22"
+    },
+    {
+      direction   = "inbound"
+      ip_version  = "IPv4"
+      remote      = "103.22.200.0/22"
+    },
+    {
+      direction   = "inbound"
+      ip_version  = "IPv4"
+      remote      = "103.31.4.0/22"
+    },
+    {
+      direction   = "inbound"
+      ip_version  = "IPv4"
+      remote      = "141.101.64.0/18"
+    },
+    {
+      direction   = "inbound"
+      ip_version  = "IPv4"
+      remote      = "108.162.192.0/18"
+    },
+    {
+      direction   = "inbound"
+      ip_version  = "IPv4"
+      remote      = "190.93.240.0/20"
+    },
+    {
+      direction   = "inbound"
+      ip_version  = "IPv4"
+      remote      = "188.114.96.0/20"
+    },
+    {
+      direction   = "inbound"
+      ip_version  = "IPv4"
+      remote      = "197.234.240.0/22"
+    },
+    {
+      direction   = "inbound"
+      ip_version  = "IPv4"
+      remote      = "198.41.128.0/17"
+    },
+    {
+      direction   = "inbound"
+      ip_version  = "IPv4"
+      remote      = "162.158.0.0/15"
+    },
+    {
+      direction   = "inbound"
+      ip_version  = "IPv4"
+      remote      = "104.16.0.0/13"
+    },
+    {
+      direction   = "inbound"
+      ip_version  = "IPv4"
+      remote      = "104.24.0.0/14"
+    },
+    {
+      direction   = "inbound"
+      ip_version  = "IPv4"
+      remote      = "172.64.0.0/13"
+    },
+    {
+      direction   = "inbound"
+      ip_version  = "IPv4"
+      remote      = "131.0.72.0/22"
+    }
+  ]
+}
+
 variable "vpc_cidr_blocks" {
   description = "List of CIDR blocks for Address Prefix"
   default = [
@@ -127,7 +209,8 @@ resource "ibm_is_security_group_rule" "sg-rule-inbound-icmp" {
   }
 }
 
-# Allow incoming ICMP packets (pings).
+# Allow incoming ICMP packets (Ping)
+##############################################################################
 resource "ibm_is_security_group_rule" "sg-rule-inbound-https" {
   group     = ibm_is_vpc.vpc.default_security_group
   direction = "inbound"
@@ -139,6 +222,7 @@ resource "ibm_is_security_group_rule" "sg-rule-inbound-https" {
   }
 }
 
+# SSH Inbound Rule
 ##############################################################################
 resource "ibm_is_security_group_rule" "sg-rule-inbound-ssh" {
   group     = ibm_is_vpc.vpc.default_security_group
@@ -148,6 +232,32 @@ resource "ibm_is_security_group_rule" "sg-rule-inbound-ssh" {
   tcp {
     port_min = 22
     port_max = 22
+  }
+}
+
+##############################################################################
+# https://cloud.ibm.com/kubernetes/api/proxy
+
+resource "ibm_is_security_group" "sg-cis-cloudflare" {
+  name = "cloudflare-security-group"
+  vpc  = ibm_is_vpc.vpc.id
+}
+
+resource "ibm_is_security_group_rule" "sg-rule-inbound-cloudflare" {
+  group     = ibm_is_security_group.sg-cis-cloudflare
+
+  dynamic "sgs" {
+    for_each = var.vpc_sg_cloudflare_rules
+
+    content {
+      direction = sgs.value.direction
+      remote    = sgs.value.remote
+    }
+  }
+
+  tcp {
+    port_min = 443
+    port_max = 443
   }
 }
 
