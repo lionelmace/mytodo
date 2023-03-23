8733cfaef046136dab54d1d3ee4e9ad559b4bd3c
Lionel Mace <lionel.mace@gmail.com>
2023-03-23 15:53:40 +0100

    finally

--- cloud/terraform/vpc-network.tf
+++ cloud/terraform/vpc-network.tf
@@ -41,18 +41,6 @@ variable "vpc_acl_rules" {
   ]
 }
 
-# CIS / Cloudflare IP Ranges 
-# https://api.cis.cloud.ibm.com/v1/ips
-variable "cis_ips" {
-  description = "List of CIS Cloudflare IPs"
-  default = [
-    "173.245.48.0/20","103.21.244.0/22","103.22.200.0/22",
-    "103.31.4.0/22","141.101.64.0/18","108.162.192.0/18",
-    "190.93.240.0/20","188.114.96.0/20","197.234.240.0/22",
-    "198.41.128.0/17","162.158.0.0/15","104.16.0.0/13",
-    "104.24.0.0/14","172.64.0.0/13","131.0.72.0/22"]
-}
-
 variable "vpc_cidr_blocks" {
   description = "List of CIDR blocks for Address Prefix"
   default = [
@@ -155,7 +143,7 @@ resource "ibm_is_security_group_rule" "sg-rule-inbound-https" {
 # SSH Inbound Rule
 ##############################################################################
 resource "ibm_is_security_group_rule" "sg-rule-inbound-ssh" {
-  group     = ibm_is_vpc.vpc.default_security_group
+  group     = ibm_is_vpc.vpc.default_security_group.id
   direction = "inbound"
   remote    = "0.0.0.0/0"
 
@@ -165,8 +153,18 @@ resource "ibm_is_security_group_rule" "sg-rule-inbound-ssh" {
   }
 }
 
+# CIS Cloudflare IPs
+# https://api.cis.cloud.ibm.com/v1/ips
 ##############################################################################
-# https://cloud.ibm.com/kubernetes/api/proxy
+variable "cis_ips" {
+  description = "List of CIS Cloudflare IPs"
+  default = [
+    "173.245.48.0/20","103.21.244.0/22","103.22.200.0/22",
+    "103.31.4.0/22","141.101.64.0/18","108.162.192.0/18",
+    "190.93.240.0/20","188.114.96.0/20","197.234.240.0/22",
+    "198.41.128.0/17","162.158.0.0/15","104.16.0.0/13",
+    "104.24.0.0/14","172.64.0.0/13","131.0.72.0/22"]
+}
 
 resource "ibm_is_security_group" "sg-cis-cloudflare" {
   name = "cloudflare-security-group"
@@ -183,22 +181,6 @@ resource "ibm_is_security_group_rule" "sg-rule-inbound-cloudflare" {
     port_max = 443
   }
 }
-# resource "ibm_is_security_group_rule" "sg-rule-inbound-cloudflare" {
-#   group     = ibm_is_security_group.sg-cis-cloudflare
-
-#   dynamic "rules" {
-#     for_each = var.vpc_sg_cloudflare_rules
-
-#     content {
-#       direction = rules.value.direction
-#       remote    = rules.value.remote
-#       tcp {
-#         port_min = 443
-#         port_max = 443
-#       }
-#     }
-#   }
-# }
 
 
 # Network ACLs
