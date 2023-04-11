73ceb18bbb7ff24180caafae68e48957c388a622
Lionel <lionel.mace@gmail.com>
2023-04-11 23:00:24 +0200

    Update security-cis.tf

--- cloud/terraform/security-cis.tf
+++ cloud/terraform/security-cis.tf
@@ -1,11 +1,11 @@
-data "ibm_cis" "cis_instance" {
-  name = "cis-lionelmace"
-}
+#data "ibm_cis" "cis_instance" {
+#  name = "cis-lionelmace"
+#}
 
-data "ibm_cis_domain" "cis_instance_domain" {
-  domain = "example.com"
-  cis_id = ibm_cis.cis_instance.id
-}
+#data "ibm_cis_domain" "cis_instance_domain" {
+#  domain = "example.com"
+#  cis_id = ibm_cis.cis_instance.id
+#}
 
 # output "cis_domain_name" {
 #   description = "The CIS Domain name"
