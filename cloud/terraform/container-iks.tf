930458a162f8f471c8c3a13fb6106f25d75d29f6
Lionel Mace <lionel.mace@gmail.com>
2023-05-05 11:13:56 +0200

    add commented

--- cloud/terraform/container-iks.tf
+++ cloud/terraform/container-iks.tf
@@ -63,9 +63,9 @@ resource "ibm_container_vpc_cluster" "iks_cluster" {
   name              = format("%s-%s", var.prefix, var.iks_cluster_name)
   vpc_id            = ibm_is_vpc.vpc.id
   resource_group_id = local.resource_group_id
+  # Optional: Specify Kubes version. If not included, default version is used
   kube_version      = var.iks_version
   cos_instance_crn  = var.is_openshift_cluster ? ibm_resource_instance.cos_openshift_registry[0].id : null
-  # entitlement                     = var.entitlement
   tags                            = var.tags
   disable_public_service_endpoint = var.disable_public_service_endpoint
   update_all_workers              = var.openshift_update_all_workers
