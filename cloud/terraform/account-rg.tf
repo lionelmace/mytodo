9c76a4452ff535206ba26fa2357f37e854c5e046
Lionel Mace <lionel.mace@gmail.com>
2023-06-13 18:18:03 +0200

    another rg fix

--- cloud/terraform/account-rg.tf
+++ cloud/terraform/account-rg.tf
@@ -20,10 +20,10 @@ resource "ibm_resource_group" "group" {
   tags  = var.tags
 }
 
-# data "ibm_resource_group" "group" {
-#   count = var.resource_group_name != "" ? 1 : 0
-#   name  = var.resource_group_name
-# }
+data "ibm_resource_group" "group" {
+  count = var.resource_group_name != "" ? 1 : 0
+  name  = var.resource_group_name
+}
 
 locals {
   # resource_group_id = var.resource_group_name != "" ? data.ibm_resource_group.group.0.id : ibm_resource_group.group.0.id
