840fbc27b7b3a6db63ff9f66b0f8d0bdd8c49c70
Lionel Mace <lionel.mace@gmail.com>
2023-08-22 11:24:12 +0200

    remove terraform scripts

--- cloud/terraform/networking-vpc-vpn.tf
+++
@@ -1,8 +0,0 @@
-# Authorization policy between VPN and Secrets Manager
-resource "ibm_iam_authorization_policy" "vpn-sm" {
-  source_service_name         = "is"
-  source_resource_type        = "vpn-server"
-  target_service_name         = "secrets-manager"
-  target_resource_instance_id = local.secrets_manager_id
-  roles                       = ["SecretsReader"]
-}
